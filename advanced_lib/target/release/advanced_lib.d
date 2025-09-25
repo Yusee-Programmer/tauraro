02d23ad235a6615756252a6465b46e442c1a6d81
Yusee-Programmer <yusee201520@gmail.com>
2025-09-25 13:08:56 +0100

    removed test files

--- advanced_lib/target/release/advanced_lib.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Yusee\ Habibu\RustProjects\tauraro\advanced_lib\target\release\advanced_lib.dll: C:\Users\Yusee\ Habibu\RustProjects\tauraro\advanced_lib\src\lib.rs
--- advanced_lib/target/release/deps/advanced_lib.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Yusee Habibu\RustProjects\tauraro\advanced_lib\target\release\deps\advanced_lib.d: src\lib.rs
-
-C:\Users\Yusee Habibu\RustProjects\tauraro\advanced_lib\target\release\deps\advanced_lib.dll: src\lib.rs
-
-src\lib.rs:
