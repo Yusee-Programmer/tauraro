215e76191efb683d83428c4105b4427205effbe7
Yusee-Programmer <yusee201520@gmail.com>
2025-09-28 17:47:06 +0100

    remove redundant files

--- hashlib-hashlib.d
+++
@@ -1,5 +0,0 @@
-hashlib-hashlib.d: src/modules/hashlib.rs
-
-libhashlib-hashlib.rmeta: src/modules/hashlib.rs
-
-src/modules/hashlib.rs:
--- test_hashlib.rs
+++
@@ -1,7 +0,0 @@
-// Simple test to verify hashlib module compiles
-fn main() {
-    println!("Testing hashlib module compilation...");
-    
-    // This is just a compilation test - we'll verify the actual functionality later
-    println!("Hashlib module should compile successfully now!");
-}
